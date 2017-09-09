c48104ecbedec3c5f65757e10b9301e746e148ee
XEonAX <sv.xeon@gmail.com>
2017-09-09 15:25:32 +0530

    Add websocket server

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+var http = require('http');
 var express = require('express');
 var app = express();
+var websocketserver = require('ws').Server;
+var env = process.env;
+var port = env.PORT || 5000;
 
-app.set('port', (process.env.PORT || 5000));
+//Register 'public' directory for static handler
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!!1');
+  response.send('Hello World!!');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running at localhost:' + app.get('port'));
+
+//create http server
+var server = http.createServer(app);
+
+
+//create websocket server on above http server
+var wss = new websocketserver({
+  server: server
 });
+
+console.log('http and ws server starting on port:' + port);
+
+//Start the http and ws servers
+server.listen(port);
+
